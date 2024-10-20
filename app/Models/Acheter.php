a0bdb2f5304f8c91263cb8bfab15fc194f4e774d
TALA-N-J <64521325+TALA-N-J@users.noreply.github.com>
2024-10-20 19:29:06 +0200

    ok

--- app/Models/Acheter.php
+++ app/Models/Acheter.php
@@ -5,7 +5,7 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
-class Acheter extends Model
+class Acheter extends Pivot
 {
     use HasFactory;
     protected $fillable = [
--- app/Models/Reserver.php
+++ app/Models/Reserver.php
@@ -5,7 +5,7 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
-class Reserver extends Model
+class Reserver extends Pivot
 {
     use HasFactory;
     protected $fillable = [
